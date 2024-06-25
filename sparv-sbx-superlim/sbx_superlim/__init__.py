4afdd7da1714839308fc68b37ca7a6de6f6e476f
Felix <felixhultin@yahoo.se>
2024-06-25 16:38:23 +0200

    first working version for argumentation sentences

--- sparv-sbx-superlim/sbx_superlim/__init__.py
+++ sparv-sbx-superlim/sbx_superlim/__init__.py
@@ -2,8 +2,10 @@
 from sparv.api import Config
 from . import superlim
 
-# __config__ = [
-#     Config("superlim.hf_model_path", "sbx/bert-base-swedish-cased-argumentation_sent", description="HuggingFace model to use for classification")
-# ]
+__config__ = [
+    # TODO: Split this variable into many different variables according to superlim task
+    # TODO: Make it configurable from config.yaml in corpus directory
+    Config("sbx_superlim.hf_model_path", "sbx/bert-base-swedish-cased-argumentation_sent", description="HuggingFace model to use for classification")
+]
 
 __description__ = "A Sparv plugin for classifying text using the Superlim baseline models."
--- sparv-sbx-superlim/sbx_superlim/superlim.py
+++ sparv-sbx-superlim/sbx_superlim/superlim.py
@@ -1,26 +1,23 @@
 """Example for a custom annotator."""
 
+from typing import List, Literal
+
 from sparv.api import Annotation, Config, Output, annotator
 from transformers import pipeline
 
 
-# @annotator("Determine if a sentence is for, against or unrelated to a topic", language="swe")
-# def argumentation(
-#     sentence: Annotation = Annotation("<sentence>"),
-#     out_stance: Output = Output("<sentence>:sbx_superlim.argumentation_stance"),
-#     out_stance_certainty: Output = Output("<sentence>:sbx_superlim.argumentation_stance_certainty"),
-#     hf_model_path: str = Config("sbx_superlim.hf_model_path")
-# ):
-#     pipe = pipeline("text-classification", model=hf_model_path)
-#     output = pipe(sentence)
-#     out_stance.write(output['label'])
-#     out_stance_certainty.write(output['score'])
-
-
-@annotator("Test annotation function", language="swe")
-def test(
+@annotator("Determine if a block of text is for, against or unrelated to a topic", language="swe")
+def argumentation(
     sentence: Annotation = Annotation("<sentence>"),
-    out: Output = Output("<sentence>:sbx_superlim.test")
+    out_stance: Output = Output("<sentence>:sbx_superlim.argumentation.stance"),
+    out_stance_certainty: Output = Output("<sentence>:sbx_superlim.argumentation.certainty"),
+    hf_model_path: str = Config("sbx_superlim.hf_model_path")
 ):
-    print("alskdjklasjdlkasjd")
-    out.write("test")
+    # TODO: Add as argument and automatically fetch allowed topics from from HF
+    topic : List[Literal['abort', 'minimilön', 'marijuanalegalisering', 'dödsstraff', 'kärnkraft', 'kloning']] = 'abort'
+    # TODO: Run in batches
+    pipe = pipeline("text-classification", model=hf_model_path)
+    # TODO: Temporary string formatting hack until training scripts and models are fixed
+    output = pipe([f"{s} $ {topic}" for s in sentence.read()])
+    out_stance.write([f"{topic}.{o['label']}" for o in output])
+    out_stance_certainty.write([str(o['score']) for o in output])
