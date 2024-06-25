4543cdf185bfd393d1a43fe0f3a67c97b0e9cb0d
Felix <felixhultin@yahoo.se>
2024-06-25 17:21:39 +0200

    add placeholder annotating functions for all tasks with training data

--- sparv-sbx-superlim/sbx_superlim/superlim.py
+++ sparv-sbx-superlim/sbx_superlim/superlim.py
@@ -21,3 +21,73 @@ def argumentation(
     output = pipe([f"{s} $ {topic}" for s in sentence.read()])
     out_stance.write([f"{topic}.{o['label']}" for o in output])
     out_stance_certainty.write([str(o['score']) for o in output])
+
+
+@annotator("Label the sentiment towards immigration on a continuous 1--5 scale", language="swe")
+def absabank_imm(
+    sentence: Annotation = Annotation("<sentence>")
+):
+    raise NotImplementedError("Sparv use case not yet defined.")
+
+
+@annotator("Determine whether a sentence is correct Swedish or not")
+def dalaj_ged(
+    sentence: Annotation = Annotation("<sentence>")
+):
+    raise NotImplementedError("Sparv use case not yet defined.")
+
+
+@annotator("Determine how related two words are on a continuous scale from 0 to 10")
+def supersim_relatedness(
+    tokens: Annotation = Annotation("<token>")
+):
+    raise NotImplementedError("Sparv use case not yet defined.")
+
+
+@annotator("Determine how similar two words are on a continuous scale from 0 to 10")
+def supersim_similarity(
+    tokens: Annotation = Annotation("<token>")
+):
+    raise NotImplementedError("Sparv use case not yet defined.")
+
+
+@annotator("Given a word pair A:B and a word C, find a word D such that A:B = C:D")
+def sweanalogy(
+    tokens: Annotation = Annotation("<token>")
+):
+    raise NotImplementedError("Sparv use case not yet defined.")
+
+
+@annotator("Given a newspaper article, provide its summary")
+def swedn(
+    tokens: Annotation = Annotation("<token>")
+):
+    raise NotImplementedError("Sparv use case not yet defined.")
+
+
+@annotator("Given the question, find the suitable answer within the same category")
+def swefaq(
+    tokens: Annotation = Annotation("<token>")
+):
+    raise NotImplementedError("Sparv use case not yet defined.")
+
+
+@annotator("Select the correct synonym")
+def swesat_synonyms(
+    tokens: Annotation = Annotation("<token>")
+):
+    raise NotImplementedError("Sparv use case not yet defined.")
+
+
+@annotator("Determine if the target word in two contexts expresses the same sense.")
+def swewic(
+    tokens: Annotation = Annotation("<token>")
+):
+    raise NotImplementedError("Sparv use case not yet defined.")
+
+
+@annotator("Determine the logical relation between two sentences", language="swe")
+def swenli(
+    sentence: Annotation = Annotation("<sentence>")
+):
+    raise NotImplementedError("Sparv use case not yet defined.")
