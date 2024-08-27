f5a1e8a99ab97205a16f0af653b86c31e56e6f72
Felix <felixhultin@yahoo.se>
2024-08-27 23:10:57 +0200

    add swenli annotator annotating entailment relationship between a sentence and its previous sentence (n-1)

--- sparv-sbx-superlim/sbx_superlim/superlim.py
+++ sparv-sbx-superlim/sbx_superlim/superlim.py
@@ -81,9 +81,26 @@ def dalaj_ged(
     out_label.write(labels)
     out_certainty.write([str(o['score']) for o in output])
 
+@annotator("Determine the logical relation between two sentences", language="swe")
+def swenli(
+    text : Text = Text(),
+    sentence: Annotation = Annotation("<sentence>"),
+    out_label : Output = Output("<sentence>:sbx_superlim.swenli.label"),
+    out_certainty: Output = Output("<sentence>:sbx_superlim.swenli.certainty"),
+    hf_model_path: str = Config("sbx_superlim.hf_model_path.swenli")
+):
+    ds_config = get_dataset_config_info('sbx/superlim-2', 'swenli')
+    inputs = prepare_inputs(text, sentence, input_type = 'sentence_pair')
+    pipe = pipeline("text-classification", model=hf_model_path)
+    output = pipe(inputs)
+    label_mapper = get_label_mapper(ds_config, pipe.model.config)
+    labels = ["N/A"] + [label_mapper[o['label']] for o in output]
+    out_label.write(labels)
+    out_certainty.write(["N/A"] + [str(o['score']) for o in output])
+
 
 @exporter("Determine the logical relation between two sentences", language="swe")
-def swenli(
+def swenli_parallel(
     source_files: AllSourceFilenames = AllSourceFilenames(),
     hf_model_path: str = Config("sbx_superlim.hf_model_path.swenli"),
     out: Export = Export("sbx_superlim.swenli/predictions.tsv"),
@@ -108,7 +125,7 @@ def swenli(
 
 
 @exporter("Determine the logical relation between two sentences", language="swe")
-def swepar(
+def swepar_parallel(
     source_files: AllSourceFilenames = AllSourceFilenames(),
     hf_model_path: str = Config("sbx_superlim.hf_model_path.swepar"),
     out: Export = Export("sbx_superlim.swepar/predictions.tsv"),
